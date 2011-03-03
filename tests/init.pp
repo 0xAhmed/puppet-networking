59b179eae9ccefa4ce514d0bb8093d8e70a0e6c9
Ahmed El Gamil <ahmed@manhag.org>
2011-03-03 12:19:59 +0200

    edited the test

--- tests/init.pp
+++ tests/init.pp
@@ -1,1 +1,9 @@
-include networking
+node "ubuntu.localdomain" {
+  networking::interfaces { "eth0" :
+      order => '02',
+          mode => 'dhcp',
+          #    ip => '192.168.218.135',
+          #    netmask => '255.255.255.0',
+          #    gateway => '192.168.218.1'
+  }
+}
