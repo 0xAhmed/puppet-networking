806563adf9dee344301aecb18ca0f728ef90374d
Ahmed El Gamil <ahmed@manhag.org>
2011-03-06 16:49:02 +0200

    for review

--- tests/init.pp
+++ tests/init.pp
@@ -1,1 +1,13 @@
-include networking
+networking::interfaces { "eth0" :
+  order => 02,
+  mode => static,
+  ip => '192.168.96.10',
+  gateway => '192.168.96.1',
+  netmask => '255.255.255.0',
+  broadcast => '255.255.255.255'
+}
+                              
+networking::interfaces { "eth1" :
+  order => 03,
+  mode => dhcp
+}
